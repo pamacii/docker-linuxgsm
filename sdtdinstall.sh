81f0a52f1dca18d37e9543c67353dd984988b3f4
Pamacii <1823454+pamacii@users.noreply.github.com>
2021-12-19 19:58:13 -0500

    Update autoinstall and default command for sdtd for easier use

--- sdtdinstall.sh
+++ sdtdinstall.sh
@@ -6,5 +6,9 @@ if [ ! -e ~/sdtdserver ]; then
     ./sdtdserver skeleton
     echo 'branch="latest_experimental"' >> lgsm/config-lgsm/sdtdserver/sdtdserver.cfg &&\
     ./sdtdserver auto-install
-    exec ./sdtdserver start
+    # exec ./sdtdserver start
+    if [ ! -e ~/log/console/sdtdserver-console.log ]; then
+        mkdir -p ~/log/console/
+        touch ~/log/console/sdtdserver-console.log
+    fi
 fi
